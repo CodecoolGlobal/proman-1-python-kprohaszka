65dd3345b3524a8f2bcb0a590db5191fdeb1016e
Sairieth <henrik.riki.marcz@gmail.com>
2021-04-20 11:56:39 +0200

    Add modifications to the foundations

--- server_from_apiwars.py
+++ server_from_apiwars.py
@@ -10,29 +10,23 @@ app.permanent_session_lifetime = False
 
 @app.route('/')
 def index():
-    planets = requests.get('https://swapi.dev/api/planets').json()['results']
-    next_page = requests.get('https://swapi.dev/api/planets').json()['next']
-    previous_page = requests.get('https://swapi.dev/api/planets').json()['previous']
     if 'user' in session:
-        return render_template('index.html', planets=planets, next=next_page, previous=previous_page)
+        return render_template('index.html')
     else:
-        return render_template('index.html', planets=planets, next=next_page, previous=previous_page)
+        return render_template('index.html')
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    planets = requests.get('https://swapi.dev/api/planets').json()['results']
-    next_page = requests.get('https://swapi.dev/api/planets').json()['next']
-    previous_page = requests.get('https://swapi.dev/api/planets').json()['previous']
     if request.method == 'POST':
         usr_name = request.form['usr_name']
         password = request.form['password']
         hashed_pass = data_manager.get_usr_credentials(usr_name)
         if util.verify_password(password, hashed_pass['password']):
             session['user'] = usr_name
-            return render_template('index.html', planets=planets, next=next_page, previous=previous_page)
+            return render_template('index.html')
     else:
-        return render_template('index.html', planets=planets, next=next_page, previous=previous_page)
+        return render_template('index.html')
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -40,7 +34,7 @@ def register():
     usr_name = request.form['usr_name']
     password = util.hash_password(request.form['password'])
     data_manager.register_usr(usr_name, password)
-    return render_template('index.html', planets=planets, next=next_page, previous=previous_page)
+    return render_template('index.html')
 
 
 @app.route('/logout')
