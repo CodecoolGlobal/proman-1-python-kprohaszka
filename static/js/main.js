2c988705fb356a03f62ab17abb96a6af26cdaa77
azarath1 <ar2oww@gmail.com>
2021-04-21 16:01:32 +0200

    add: add board button toggle function

--- static/js/main.js
+++ static/js/main.js
@@ -6,7 +6,20 @@ function init() {
     dom.init();
     // loads the boards to the screen
     dom.loadBoards();
-
+    addBoardButtonToggle();
 }
 
 window.onload = init
+
+
+// ADD Board
+function addBoardButtonToggle()
+{
+    let addBoardButton = document.getElementById('add-board');
+    addBoardButton.addEventListener('click', addNewBoard);
+}
+
+
+function addNewBoard() {
+    document.getElementById('add-board').disabled = true;
+}
