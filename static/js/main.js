95273a11ad363231bda0125a7ec9378fd02da88c
azarath1 <ar2oww@gmail.com>
2021-04-22 07:38:15 +0200

    add: event listeners for add board buttons TODO: Fix query and render

--- static/js/main.js
+++ static/js/main.js
@@ -6,11 +6,11 @@ function init() {
     dom.init();
     // loads the boards to the screen
     dom.loadBoards();
+    // adds boards to the table when clicked
     addBoardButtonToggle();
 }
 
 window.onload = init
-
 
 // ADD Board
 function addBoardButtonToggle()
@@ -22,4 +22,21 @@ function addBoardButtonToggle()
 
 function addNewBoard() {
     document.getElementById('add-board').disabled = true;
+    createInputForNewBoard()
+}
+
+
+function createInputForNewBoard()
+{
+    document.getElementById('new-board-input').hidden = false;
+    let saveButton = document.getElementById('create-board');
+    let cancelButton = document.getElementById('cancel-board');
+    //saveButton.addEventListener('click', );
+    cancelButton.addEventListener('click', closeForm);
+}
+
+function closeForm() {
+    document.getElementById('new-board-input').hidden = true;
+    document.getElementById('new-board-input').value = '';
+    document.getElementById('add-board').disabled = false;
 }
