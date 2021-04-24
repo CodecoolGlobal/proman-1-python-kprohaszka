6130571248e0d3747d1c5dda4f457f3bb4719170
azarath1 <ar2oww@gmail.com>
2021-04-24 09:49:30 +0200

    add: data_handler sql query for delete cards

--- data_handler.py
+++ data_handler.py
@@ -79,6 +79,7 @@ def add_new_card(cursor: RealDictCursor, board_id: int, title: str, status_id):
     args = {'board_id': board_id, 'title': title, 'status_id': status_id}
     cursor.execute(query, args)
 
+
 @database_common.connection_handler
 def save_changed_card(cursor: RealDictCursor, card_id:int, status_id:int):
     query = """
@@ -88,3 +89,11 @@ def save_changed_card(cursor: RealDictCursor, card_id:int, status_id:int):
     """
     args = {'card_id': card_id, 'status_id': status_id}
     cursor.execute(query, args)
+
+
+@database_common.connection_handler
+def delete_card(cursor, card_id):
+    query = """
+    DELETE FROM cards
+    WHERE id = %s"""
+    cursor.execute(query, (card_id, ))
